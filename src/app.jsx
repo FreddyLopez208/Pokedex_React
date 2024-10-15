1c150e832c556b9373bf1759424887bc08a2559c
Freddy Lopez <freddylopez208@gmail.com>
2024-10-15 22:14:40 +0100

    Pokedex terminada V1

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'preact/hooks'
+import LayaoutHome from './pages/home/layout/LayoutHome'
+
+
+export function App() {
+
+  return (
+      <div>
+       <LayaoutHome/>
+      </div>
+      
+  )
+}
